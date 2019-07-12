a2d54edfa4cf9cf6c04401beaf6db885c23e18a0
Toyz <>
2019-07-12 17:15:06 +0800

    no message

--- SpringMVC/SpringMVC.SSM/src/main/java/com/handler/AccountHandler.java
+++ SpringMVC/SpringMVC.SSM/src/main/java/com/handler/AccountHandler.java
@@ -1,14 +1,22 @@
 package com.handler;
 
+import com.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("account")
 public class AccountHandler {
+
+    // 依赖注入AccountService对象
+    @Autowired
+    private AccountService accountService;
+
     @RequestMapping("findAll")
     public String findAll(){
-        System.out.println("表现层，展示所有用户");
+        System.out.println("表现层，展示所有用户...");
+        accountService.findAll();
         return "success";
     }
 }
